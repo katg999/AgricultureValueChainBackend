5995013b2b2efeae207d0c3efffe91734fdd9441
PatienceAkugizibwe <phionanyangoma401@gmail.com>
2026-06-02 15:53:59 +0300

    adding branch batch-form

---
+++ frontend/ugaap-portal/src/app/features/branch/finance/batch-form/batch-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-batch-form',
+  imports: [],
+  templateUrl: './batch-form.html',
+  styleUrl: './batch-form.css',
+})
+export class BatchForm {}
